3ad1a1216c8a4b7c54d21dfc105f93defc761fa9
Slacker201 <slacker1234567809@gmail.com>
2025-12-03 08:52:23 -0600

    added handy fn

--- src/tokens/span.rs
+++ src/tokens/span.rs
@@ -9,4 +9,7 @@ impl Span {
     pub fn new(start: u64, end: u64) -> Span {
         Self { start, end }
     }
+    pub fn new_usize(start: usize, end: usize) -> Span {
+        Self::new(start as u64, end as u64)
+    }
 }
